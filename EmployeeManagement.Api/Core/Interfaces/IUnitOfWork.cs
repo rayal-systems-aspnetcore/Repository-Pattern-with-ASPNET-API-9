4dd8d6986170fb17dce809679246b6e9604f50e6
prayal <pankajrayal@gmail.com>
2025-11-03 08:15:17 -0600

    The Unit of Work Pattern

---
+++ EmployeeManagement.Api/Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace EmployeeManagement.Api.Core.Interfaces {
+  public interface IUnitOfWork: IDisposable {
+    IEmployeeRepository Employees { get; }
+    IDepartmentRepository Departments { get; }
+    Task<int> CompleteAsync();
+  }
+}
---
+++ EmployeeManagement.Api/Data/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagement.Api.Core.Interfaces;
+
+namespace EmployeeManagement.Api.Data {
+  public class UnitOfWork: IUnitOfWork {
+    private readonly ApplicationDbContext _context;
+    private bool _disposed = false;
+
+    public IEmployeeRepository Employees { get; private set; }
+    public IDepartmentRepository Departments { get; private set; }
+
+    public UnitOfWork(ApplicationDbContext context) {
+      _context = context;
+
+      Employees = new EmployeeRepository(_context);
+      Departments = new DepartmentRepository(_context);
+    }
+
+    public async Task<int> CompleteAsync() {
+      return await _context.SaveChangesAsync();
+    }
+
+    public virtual void Dispose(bool disposing) {
+      if(!_disposed) {
+        if(disposing) {
+          _context.Dispose();
+        }
+        _disposed = true;
+      }
+    }
+
+    public void Dispose() {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+  }
+}
